fb24e6dd9b0fc709dbff6dfc2675bbff6c700bbb
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2014-03-26 22:56:47 +0800

    added comments and cleaned codes

--- functions.R
+++ functions.R
@@ -1,18 +1,4 @@
-get.returns <- function(return.ws, country, base.date, boundry.days){
-  
-  if(country=="Brazil"){
-    country.return <- readWorksheet(return.ws, sheet=3, region="A1:E5001", header=T)
-  }
-  
-  upper.boundry <- base.date + (24*60*60)*boundry.days
-  lower.boundry <- base.date - (24*60*60)*boundry.days
-  
-  result <- country.return[upper.boundry >= country.return$Date & country.return$Date >= lower.boundry ,]
-  
-  return(result)
-}
-
-
+## function to get the match return with winner and loser with boundry days range
 get.match.return <- function(winner, loser, date, stage, boundry.days){
   
   upper.boundry <- date + (24*60*60)*boundry.days
@@ -41,22 +27,51 @@ get.match.return <- function(winner, loser, date, stage, boundry.days){
   
   match.return <- merge(winner.match.return, loser.match.return, by="Date", all=TRUE, sort=TRUE)
   
-  #if(nrow(match.return) > 0){
-  #  write.csv(match.return, 
-  #            paste("export/", format(min(date), "%Y %m %d")," ", winner, " vs ", loser, " ", stage, ".csv", sep=""), 
-  #            row.names=FALSE, 
-  #            na="")  
-  #}
-  
   return(match.return)
 }
 
 
+## function to get the match return with winner and loser with boundry days range
+## but this function will write to csv file
+## you just need to adjust the boundry.days variable in script.R
+get.match.return.file <- function(winner, loser, date, stage, boundry.days){
+  
+  upper.boundry <- date + (24*60*60)*boundry.days
+  lower.boundry <- date - (24*60*60)*boundry.days
+  
+  ## to format the data for saving csv file
+  format(min(germany[,1]), "%Y %m %d")
+  
+  if(exists(winner)){
+    eval(parse(text=paste("winner.returns <- ", winner, sep = "")))
+    winner.match.return <- winner.returns[upper.boundry >= winner.returns$Date & winner.returns$Date >= lower.boundry ,]
+  }else{
+    winner.match.return <- brazil[0,]
+  }
+  
+  colnames(winner.match.return)[2] <- "winner.closing.price"
+  
+  if(exists(loser)){
+    eval(parse(text=paste("loser.returns <- ", loser, sep = "")))
+    loser.match.return <- loser.returns[upper.boundry >= loser.returns$Date & loser.returns$Date >= lower.boundry ,]
+  }else{
+    loser.match.return <- brazil[0,]
+  }
+  
+  colnames(loser.match.return)[2] <- "loser.closing.price"
+  
+  match.return <- merge(winner.match.return, loser.match.return, by="Date", all=TRUE, sort=TRUE)
+  
+  if(nrow(match.return) > 0){
+    write.csv(match.return, 
+              paste("export/", format(min(date), "%Y %m %d")," ", winner, " vs ", loser, " ", stage, ".csv", sep=""), 
+              row.names=FALSE, 
+              na="")  
+  }
+}
 
-
-
-
-
+## function to get given country's results in 10 days range with all the match days
+## to adjust days, change line 85.
 get.country.return <- function(team){
   sheet <- fifa.result[0,]
   
--- script.R
+++ script.R
@@ -1,17 +1,21 @@
-#nstall.packages("XLConnect")
+## need to install these packages to read XLSX
+#install.packages("XLConnect")
+#install.packages("seqinr")
 library (XLConnect)
 library(seqinr)
 
+## this line will call "data import.R" file and it will import the necessary data.
 source("data import.R")
+
+## this line will load necessary functions
 source("functions.R")
 
+############### first version of files
+
 for(i in 1:nrow(fifa.result)){
-#for(i in 1:16){  
   match <- fifa.result[i,]
-  get.match.return(match[,"Winner"], match[,"Loser"], match[,"Date"], match[,"Stage"], 10)
+  get.match.return.file(match[,"Winner"], match[,"Loser"], match[,"Date"], match[,"Stage"], 10)
 }
-
-
 
 ############### second version start here
 
@@ -38,134 +42,3 @@ for(i in 1:length(teams)){
 saveWorkbook(wb)
 
 ################ end here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###################################### testng scripts 
-
-all.return <- loadWorkbook("data/All Returns.xlsx")
-
-fifa        <- loadWorkbook("data/FIFA outcomes.xlsx")
-fifa.result <- readWorksheet(fifa, sheet="Data", region="A1:F133", header=T)
-
-fifa.result[, 2:3] <- lapply(fifa.result[, 2:3], function(x) gsub(" ", ".", trimSpace(tolower(x))))
-
-winning <- unique(fifa.result[, 2])
-losing  <- unique(fifa.result[, 3])
-teams <- unique(c(winning, losing))
-
-sort(setdiff(teams, ls()))
-sort(setdiff(ls(), teams))
-
-years <- sort(unique(as.numeric(format(fifa.result[,"Date"], "%Y"))), decreasing=F)
-
-## to format the data for saving csv file
-format(min(germany[,1]), "%Y %m %d")
-
-## to call the object from string
-eval(parse(text=paste("df$", x, sep = "")))
-
-brazil.return <- get.returns(return.ws=all.return, 
-                             country=fifa.result[14,"Winner"],
-                             base.date=fifa.result[14,"Date"], 
-                             boundry.days=10)
-
-brazil <- loadWorkbook("data/brazil.xlsx")
-brazil <- readWorksheet(brazil, sheet=1, region="A1:E5001", header=T)
-
-germany <- loadWorkbook("data/germany.xlsx")
-germany <- readWorksheet(germany, sheet=1, header=T)
-
-
-brazil.winning.dates <- fifa.result[fifa.result[,2] == "Brazil", "Date"]
-brazil.losing.dates  <- fifa.result[fifa.result[,3] == "Brazil", "Date"]
-
-brazil.playing.dates <- sort(c(brazil.winning.dates, brazil.losing.dates), decreasing=F)
-
-brazil.return <- 0
-boundry.days  <- 10
-country.return <- brazil
-
-for(i in 1:length(brazil.playing.dates)){
-  base.date <- brazil.playing.dates[i]
-  
-  upper.boundry <- base.date + (24*60*60)*boundry.days
-  lower.boundry <- base.date - (24*60*60)*boundry.days
-  
-  result <- country.return[upper.boundry >= country.return$Date & country.return$Date >= lower.boundry ,]
-  
-  if(i == 1){
-    brazil.return <- result
-  }else{
-    brazil.return <- rbind(brazil.return, result)
-  }
-}
-
-## order the brazil returns and remove the duplicates
-brazil.return <- brazil.return[order(brazil.return$Date, decreasing=F),]
-brazil.return <- brazil.return[!duplicated(brazil.return$Date),]
-
-## writing csv file
-write.csv(brazil.return, "brazil returns.csv", row.names=FALSE, na="")
-
-## get only 2002 data for testing
-brazil.2002 <- brazil.return[as.numeric(format(brazil.return[,"Date"], "%Y"))==2002, ]
-
-## plot to test
-plot(brazil.2002$Close, type="l", xaxt="n")
-axis(1, at=1:nrow(brazil.2002), labels=brazil.2002[,"Date"])
-
-
-## check this potion till write to csv
-
-sample.final.date <- fifa.result[14,"Date"]
-
-upper.boundry <- sample.final.date + (24*60*60)*boundry.days
-lower.boundry <- sample.final.date - (24*60*60)*boundry.days
-
-germany.result <- germany[upper.boundry >= germany$Date & germany$Date >= lower.boundry ,]
-brazil.result  <- brazil[upper.boundry >= brazil$Date & brazil$Date >= lower.boundry ,]
-
-final.2002 <- merge(brazil.result, germany.result, by="Date", all=TRUE, sort=TRUE)
-
-## writing csv file
-write.csv(final.2002, "2002 final returns.csv", row.names=FALSE, na="")
-
-## plot to test
-
-
-brazil <- loadWorkbook("data/All Returns copy 2.xlsx")
-brazil <- readWorksheet(brazil, sheet=1, header=T)
-class(brazil[,1])
-
-match <- fifa.result[16,]
-get.match.return(match[,2], match[,3], match[,6], match[,1], 10)
-
-
-filename <- "myWorkbook.xlsx"
-sheetname <- "data1"
-data.to.write <- germany
-
-wb <- loadWorkbook(filename, create = TRUE)
-createSheet(wb, name = sheetname)
-writeWorksheet(wb, data.to.write, sheet = sheetname)
-
-createSheet(wb, name = "data2")
-writeWorksheet(wb, romania, sheet = "data2")
-
-
-saveWorkbook(wb)
